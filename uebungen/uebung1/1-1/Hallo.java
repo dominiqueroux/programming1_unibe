649690a09fa85dcdd46356f89e6ba745ee521e09
chaotusmaugus <chaotusmaugus@bluewin.ch>
2016-10-06 14:59:14 +0200

    Update Hallo.java
    Commentsection

--- uebungen/uebung1/1-1/Hallo.java
+++ uebungen/uebung1/1-1/Hallo.java
@@ -1,3 +1,12 @@
+/*
+******************************************************
+HS Programming 1
+1_1
+Dominique Roux, 16-100-661
+Marc Bachofner, 15-135-916
+*******************************************************
+*/ 
+
 public class Hallo {
     public static void main (String[] args) {
         System.out.println("Hallo Welt!");
