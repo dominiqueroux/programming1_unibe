8a407d9208015a0de464f9393e79b0ea48c2a88e
Dominique Roux <dominique@roux.li>
2016-11-30 13:03:40 +0100

    added headers

--- uebungen/uebung5/5_1/AddressFileException.java
+++ uebungen/uebung5/5_1/AddressFileException.java
@@ -1,3 +1,9 @@
+/* ************************************************************************** *\
+*                 Programmierung 1 HS 2016 - Serie 5-1  
+*                 Bachofner Marc      15-135-916
+*                 Dominique Roux      16-100-661
+\* ************************************************************************ */
+
 public class AddressFileException extends Exception
 {
     public AddressFileException( String message ) {
--- uebungen/uebung5/5_1/AddressFileLabelled.java
+++ uebungen/uebung5/5_1/AddressFileLabelled.java
@@ -1,3 +1,8 @@
+/* ************************************************************************** *\
+*                 Programmierung 1 HS 2016 - Serie 5-1  
+*                 Bachofner Marc      15-135-916
+*                 Dominique Roux      16-100-661
+\* ************************************************************************ */
 import java.util.*;
 
 public class AddressFileLabelled extends AddressFile 
