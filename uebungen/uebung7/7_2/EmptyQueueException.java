d553b88e3c31f76328e39eb2e0d4f8020ee83799
Dominique Roux <dominique.roux4@gmail.com>
2016-12-15 16:34:11 +0100

    added header

--- uebungen/uebung7/7_2/EmptyQueueException.java
+++ uebungen/uebung7/7_2/EmptyQueueException.java
@@ -1,3 +1,9 @@
+/* ************************************************************************** *\
+ * *                 Programmierung 1 HS 2016 - Serie 7-2  
+ * *                 Bachofner Marc      15-135-916
+ * *                 Dominique Roux      16-100-661
+ * \* ************************************************************************ */
+
 public class EmptyQueueException extends Exception
 {
     public EmptyQueueException() {
